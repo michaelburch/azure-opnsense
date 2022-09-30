1a746b81f66e84a779169964ef4dafc680329631
Michael <me@michaelburch.net>
2022-09-30 08:38:01 -0500

    add params

--- install-opnsense.sh
+++ install-opnsense.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
-OPNSENSE_VERSION="22.7"
-AZURE_AGENT_VERSION="2.8.0.11"
+OPNSENSE_VERSION="${3:-22.7}"
+AZURE_AGENT_VERSION="${4:-2.8.0.11}"
 
-# Fetch config and boostrap script
+# Fetch config and bootstrap script
 BOOTSTRAP_URI="${1:-https://raw.githubusercontent.com/opnsense/update/$OPNSENSE_VERSION/src/bootstrap/opnsense-bootstrap.sh.in}"
 CONFIG_URI="${2:-https://raw.githubusercontent.com/michaelburch/azure-opnsense/main/config/$OPNSENSE_VERSION-generic.xml}" 
 echo "Retrieving $CONFIG_URI"
@@ -22,8 +22,8 @@ fetch https://github.com/Azure/WALinuxAgent/archive/refs/tags/v$AZURE_AGENT_VERS
 tar -xvzf v$AZURE_AGENT_VERSION.tar.gz
 cd WALinuxAgent-$AZURE_AGENT_VERSION/
 python3 setup.py install --register-service --lnx-distro=freebsd --force
-cd..
-ln -s /usr/local/bin/python3.9 /usr/local/bin/python
+cd ..
+ln -sf /usr/local/bin/python3.9 /usr/local/bin/python
 sed -i "" 's/ResourceDisk.EnableSwap=y/ResourceDisk.EnableSwap=n/' /etc/waagent.conf
 
 # Reboot
